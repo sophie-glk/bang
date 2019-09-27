d040dc99a4bf5f305b8de5189467e994d5c0b230
tt7753 <tobiasglueck@mailbox.org>
2019-09-28 00:55:48 +0200

    Updated bangs.json. Changed how auto update works

--- plugin/update.js
+++ plugin/update.js
@@ -1,7 +1,10 @@
 var branch = "develop";
+/*
+ check_for_banglist_update();
 setInterval(function() {
-   check_for_banglist_update();
- }, 1000 * 60 * 30);
+    check_for_banglist_update();
+}, 1000 * 60 * 30);
+*/
 function check_for_banglist_update() {
     chrome.storage.sync.get(['banglist_version'], function(result) {
         var version = result.banglist_version;
@@ -22,28 +25,36 @@ function check_for_banglist_update() {
     });
 
     function check(curr_version) {
-        get_json("https://raw.githubusercontent.com/sophie-glk/bang/"+branch+"/plugin/banglist/version.json", function(response) {
+        get_json("https://raw.githubusercontent.com/sophie-glk/bang/" + branch + "/plugin/banglist/version.json", function(response) {
             var new_version = response.version;
             console.log(new_version);
             if (new_version > curr_version) {
-                update_banglist();
+                update_banglist(curr_version);
             }
         });
-       
+
 
     }
 
 }
 
-function update_banglist() {
-     get_json("https://raw.githubusercontent.com/sophie-glk/bang/"+branch+"/plugin/banglist/bangs.json", function(response) {
-         if(response != null && Array.isArray(response)){
-         console.log("Update successful!");
-         var bl = JSON.stringify(response);
-         chrome.storage.sync.set({
-                    "banglist": bl
-                });
-         }
+function update_banglist(to_version) {
+    get_json("https://duckduckgo.com/bang.v" + to_version + ".js", function(response) {
+        if (response != null && Array.isArray(response)) {
+            var result = [];
+            for (var i = 0; i < response.length; i++) {
+                var bang_site = response[i].u.replace("{{{s}}}", "bang");
+                var bang_prefix = "!" + response[i].t;
+                result.push([bang_prefix, bang_site]);
+            }
+            console.log(result);
+            console.log("Update successful!");
+            var bl = JSON.stringify(result);
+            chrome.storage.sync.set({
+                "banglist": bl
+            });
+
+        }
     });
 
 }
