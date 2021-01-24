16d77964f39c79b11e928d467380abcf7bf56bb1
tt7753 <tobiasglueck@mailbox.org>
2021-01-24 19:02:12 +0100

    created shared_functions.js

--- plugin/background.js
+++ plugin/background.js
@@ -99,47 +99,5 @@ function match(url, search, tab_id) {
     }
 }
 
-function has_prefix(bang, prefix){
-    if(prefix == "" || prefix == null || prefix == " "){
-     prefix = "!";
-    }
-     if(bang[0] == prefix && bang[1] != "" && bang[1] != null){
-         return true;
-    }
-    else{ return false;}
-    
-}
-
-function get_prefix(callback){
-     chrome.storage.sync.get(['prefix'], function(result) {
-      callback(result.prefix);});
-}
-
-function contains_possible_bang(search, callback){
-     get_prefix(function(prefix) {
-    var b = false;
-    var bang = "";
-    var s = "";
-    var words = search.split(" ");
-    for (var i = 0; i < words.length; i++){
-        word = words[i];
-        if(has_prefix(word, prefix)){
-            var rpl = word;
-            if( words.length > 1){
-            var space = " ";
-            rpl = word + space;
-            if(i > 0){
-               rpl = space + word; 
-            }
-            }
-            s = search.replace(rpl, "");
-            b = true;
-            bang = word;
-            break;
-        }
-    }
-    callback(b, s, bang);
-     }); 
-}
 
 
