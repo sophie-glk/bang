12edf6064ddc72d5ae502f68b54c877caecd7aa7
sophie-glk <sophieglueck@mailbox.org>
2021-01-24 19:32:58 +0100

    Delete ecosia.js

--- plugin/ecosia.js
+++
@@ -1,9 +0,0 @@
-intercept_url("/search?q=");
-var form = document.getElementsByClassName("search-form")[0];
-var button = document.getElementsByClassName("button-submit")[0];
-var search_url = "https://" + window.location.hostname + "/search?q=@search@"
-intercept_engine({form: form, button: button, getsearch: getsearch, search_url: search_url, fix_btn: true});
-
-function getsearch() {
-	return document.getElementsByName("q")[0].value;
-}
