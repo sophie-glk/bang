0d78a30f215fd3d7d0e556e519c13171df9cf6fa
soph.glk <sophieglueck@mailbox.org>
2024-05-31 16:51:47 +0200

    Revert "clean up code"
    This reverts commit 4291bb06a63f942f284aee2b791e1114cfdd8ce6.

--- plugin/bang.js
+++ plugin/bang.js
@@ -2,13 +2,14 @@ browser.storage.sync.set({
     "banglist": ""
 });
 
-chrome.storage.local.get(['version'], function (result) {
-    let manifestVer = chrome.runtime.getManifest().version;
-    if (result.version == null || result.version < manifestVer) {
+browser.storage.local.get(['version'], function (result) {
+    var manv = browser.runtime.getManifest().version;
+    if (result.version == null || result.version < manv) {
         console.log("reset banglist");
-        chrome.storage.local.set({
+
+        browser.storage.local.set({
             "banglist": "",
-            "version": manifestVer
+            "version": manv
         });
     }
 
@@ -26,119 +27,126 @@ browser.runtime.onMessage.addListener(function (request, sender) {
 
 function bang(request, tab_id) {
     var search = request.srch;
-    if (search == null) { return; }
     var raw_search = request.raw_srch;
     var replace = request.rpl;
     var bang = request.bng;
     bang = "!" + bang.substring(1);
     console.log(bang);
     var search_url = request.srch_url;
-    chrome.storage.sync.get(['bangs', 'onlycustom']).then((result) => {
-        if (result.bangs != null) {
-            bangs = JSON.parse(result.bangs);
-        }
-        //check local bang list and their aliases
-        if (bangs != null && bangs.length != 0) {
-            for (let i = 0; i < bangs.length; i++) {
-                let bang_alias = bangs[i].bang.split(" ");
-                for (j = 0; j < bang_alias.length; j++) {
-                    if (bang == "!" + bang_alias[j]) {
-                        use_bang(bangs[i].url, raw_search, "@search@");
-                        return;
-                    }
-                }
+    if (search != null) {
+        browser.storage.sync.get(['bangs', 'onlycustom'], function (result) {
+            var bangs = null;
+            if (result.bangs != null) {
+                bangs = JSON.parse(result.bangs);
             }
-        }
-        if (result.onlycustom != true) {
-            checklocal();
-        }
-    });
-}
+            let found = false;
+            if (bangs != null && bangs.length != 0) {
+                for (let i = 0; i < bangs.length; i++) {
+                    var bang_alias = bangs[i].bang.split(" ");
+                    for (j = 0; j < bang_alias.length; j++) {
+                        if (bang == "!" + bang_alias[j]) {
+                            found = true;
+                            use_bang(bangs[i].url, raw_search, "@search@");
+                            break;
+                        }
+                    }
+                    if (found) break;
+                }
 
-function checklocal() {
-    chrome.storage.local.get(['banglist'], function (result) {
-        let banglist = result.banglist;
-        if (banglist == null || banglist == "") {
-            console.log("first run");
-            load_bangsjson(check);
-        } else {
-            check(banglist);
-        }
-    });
+            }
 
-    function load_bangsjson(check) {
-        get_file(chrome.runtime.getURL('banglist/bangs.json'), "json", function (response) {
-            var bl = JSON.stringify(response);
-            chrome.storage.local.set({
-                "banglist": bl
-            });
-            check(bl);
+            if (!found && search != null && result.onlycustom != true) {
+                checklocal();
+            }
+
+        });
+    }
+
+    function checklocal() {
+        browser.storage.local.get(['banglist'], function (result) {
+            var banglist = result.banglist;
+            if (banglist == null || banglist == "") {
+                console.log("first run");
+                load_bangsjson(check);
+            } else {
+                check(banglist);
+            }
         });
 
+        function load_bangsjson(check) {
+            get_file(browser.runtime.getURL('banglist/bangs.json'), "json", function (response) {
+                var bl = JSON.stringify(response);
+                browser.storage.local.set({
+                    "banglist": bl
+                });
+                check(bl);
+            });
+
+        }
+
+        function check(bl) {
+            var found = false;
+            var banglist = JSON.parse(bl);
+            for (var i = 0; i < banglist.length; i++) {
+                if ("!" + banglist[i].t == bang.toLowerCase()) {
+                    console.log("using local list");
+                    use_bang(banglist[i].u, raw_search, "{{{s}}}");
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                normalsearch();
+            }
+
+        }
+
     }
 
-    function check(bl) {
-        var found = false;
-        var banglist = JSON.parse(bl);
-        for (var i = 0; i < banglist.length; i++) {
-            if ("!" + banglist[i].t == bang.toLowerCase()) {
-                console.log("using local list");
-                use_bang(banglist[i].u, raw_search, "{{{s}}}");
-                found = true;
-                break;
+    function normalsearch() {
+        if (search_url != null) {
+            var URL = search_url.replace("@search@", encodeURIComponent(search));
+            update_tab(URL);
+        }
+    }
+
+    function update_tab(url) {
+        // `!blogspot` returns `/?q={{{s}}}+site:blogspot.com`
+        // Turn relative URLs like above into absolute URLs
+        // Already absolute URLs are left unchanged
+        url = new URL(url, 'https://duckduckgo.com/').href;
+        browser.runtime.getPlatformInfo().then((info) => {
+            let updateProperties;
+            //android doesn't support the loadReplace option.
+            if (info.os == "android") {
+                updateProperties = { url };
+            }
+            else {
+                updateProperties = {
+                    loadReplace: replace,
+                    url
+                };
+            }
+            if (tab_id != null) {
+                browser.tabs.update(tab_id, updateProperties);
+            } else {
+                browser.tabs.update(updateProperties);
             }
         }
-        if (!found) {
-            normalsearch();
-        }
+
+        );
+
 
     }
 
-}
-
-function normalsearch() {
-    if (search_url != null) {
-        var URL = search_url.replace("@search@", encodeURIComponent(search));
-        update_tab(URL);
-    }
-}
-
-function update_tab(url) {
-    // `!blogspot` returns `/?q={{{s}}}+site:blogspot.com`
-    // Turn relative URLs like above into absolute URLs
-    // Already absolute URLs are left unchanged
-    url = new URL(url, 'https://duckduckgo.com/').href;
-    browser.runtime.getPlatformInfo().then((info) => {
-        let updateProperties;
-        //android doesn't support the loadReplace option.
-        if (info.os == "android") {
-            updateProperties = { url };
-        }
+    function use_bang(bang_url, raw_search, id) {
+        var url = "";
+        if (raw_search != "") { url = bang_url.replace(id, encodeURIComponent(raw_search)); }
         else {
-            updateProperties = {
-                loadReplace: replace,
-                url
-            };
+            var u = new URL(bang_url);
+            url = u.protocol + "//" + u.hostname;
         }
-        if (tab_id != null) {
-            chrome.tabs.update(tab_id, updateProperties);
-        } else {
-            chrome.tabs.update(updateProperties);
-        }
+        update_tab(url);
     }
 
-    );
-
-
 }
-
-function use_bang(bang_url, raw_search, id) {
-    var url = "";
-    if (raw_search != "") { url = bang_url.replace(id, encodeURIComponent(raw_search)); }
-    else {
-        var u = new URL(bang_url);
-        url = u.protocol + "//" + u.hostname;
-    }
-    update_tab(url);
-}
-
--- plugin/shared_functions.js
+++ plugin/shared_functions.js
@@ -1,46 +1,45 @@
-function get_prefix(callback) {
-     chrome.storage.sync.get(['prefix'], function (result) {
-          callback(result.prefix);
-     });
+function get_prefix(callback){
+     chrome.storage.sync.get(['prefix'], function(result) {
+      callback(result.prefix);});
 }
 
-function has_prefix(bang, prefix) {
-     if (prefix == "" || prefix == null || prefix == " ") {
-          prefix = "!";
-     }
-     if (bang[0] == prefix && bang[1] != "" && bang[1] != null) {
-          return true;
-     }
-     else { return false; }
+function has_prefix(bang, prefix){
+    if(prefix == "" || prefix == null || prefix == " "){
+     prefix = "!";
+    }
+     if(bang[0] == prefix && bang[1] != "" && bang[1] != null){
+         return true;
+    }
+    else{ return false;}
 }
 
-function contains_possible_bang(search, callback) {
-     get_prefix(function (prefix) {
-          if (prefix == null) {
-               prefix = "!";
-          }
-          var b = false;
-          var bang = "";
-          var s = "";
-          var words = search.split(prefix);
-          if (words.length > 1) {
-               var pb = words[1].split(" ");
-               var word = prefix + pb[0];
-               if (word != null && word.length > 1) {
-                    var space = " ";
-                    var rpl = space + word;
-                    if (words.length == 2 && pb.length == 1) {
-                         rpl = word;
-                    }
-
-                    else if (words[0] == "") {
-                         rpl = word + space;
-                    }
-                    s = search.replace(rpl, "");
-                    b = true;
-                    bang = word;
-               }
-          }
-          callback(b, s, bang);
+function contains_possible_bang(search, callback){
+     get_prefix(function(prefix) {
+    if(prefix == null){
+     prefix = "!";
+    }
+    var b = false;
+    var bang = "";
+    var s = "";
+    var words = search.split(prefix);
+            if(words.length > 1){
+            var pb = words[1].split(" ");
+            var word = prefix + pb[0];
+            if( word != null && word.length > 1){
+            var space = " ";
+            var rpl = space + word;
+            if(words.length == 2 && pb.length == 1){
+            rpl = word;    
+            }
+            
+            else if(words[0] ==  ""){
+               rpl = word + space; 
+            }
+            s = search.replace(rpl, "");
+            b = true;
+            bang = word;
+            }
+            }
+    callback(b, s, bang);
      });
 }
